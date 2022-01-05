7458906e2038365eb9a0278baa4a04b9b66ddb0a
Ryan Ferrell <importRyan@gmail.com>
2022-01-05 15:13:18 -0800

    Update to latest SDK logging methods

--- Shared/Screens/4_Action/VMs/MetaWear+WriteSensorMacro.swift
+++ Shared/Screens/4_Action/VMs/MetaWear+WriteSensorMacro.swift
@@ -5,18 +5,17 @@ import MetaWear
 import Combine
 
 public extension AnyPublisher where Output == MetaWear {
-#warning("Update Optionally Log for Pollables")
     func macro(_ config: ModulesConfiguration) -> MWPublisher<MWMacroIdentifier> {
         self.macro(executeOnBoot: true) { metawear in
             metawear
                 .optionallyLog(config.accelerometer)
                 .optionallyLog(config.altitude)
                 .optionallyLog(config.gyroscope)
-//                .optionallyLog(config.humidity)
+                .optionallyLog(byPolling: config.humidity)
                 .optionallyLog(config.ambientLight)
                 .optionallyLog(config.magnetometer)
                 .optionallyLog(config.pressure)
-//                .optionallyLog(config.thermometer)
+                .optionallyLog(byPolling: config.thermometer)
                 .optionallyLog(config.fusionEuler)
                 .optionallyLog(config.fusionGravity)
                 .optionallyLog(config.fusionLinear)
