0dd238e0413a52ba7ecc433d822e89b3d95f169b
Ryan Ferrell <importRyan@gmail.com>
2022-01-06 10:04:07 -0800

    Add iOS-only press gesture to UnderlineButtonStyle

--- Shared/UI/Buttons/UnderlineButtonStyle.swift
+++ Shared/UI/Buttons/UnderlineButtonStyle.swift
@@ -11,7 +11,11 @@ struct UnderlinedButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .contentShape(Rectangle())
-            .background(Style(config: self))
+        #if os(iOS)
+            .background(Style(config: self, isPressed: configuration.isPressed))
+        #else
+            .background(Style(config: self, isPressed: false))
+        #endif
             .scaleEffect(configuration.isPressed ? 0.96 : 1)
             .animation(.spring(), value: configuration.isPressed)
     }
@@ -19,13 +23,14 @@ struct UnderlinedButtonStyle: ButtonStyle {
     private struct Style: View {
 
         var config: UnderlinedButtonStyle
+        var isPressed: Bool
         @Environment(\.isEnabled) private var isEnabled
 
         var body: some View {
-            UnderlineRect(cornerRadius: 5, percent: config.isHovered ? 1 : 0)
+            UnderlineRect(cornerRadius: 5, percent: config.isHovered || isPressed ? 1 : 0)
                 .strokeBorder(lineWidth: 3, antialiased: true)
                 .foregroundColor(isEnabled ? config.color : .myTertiary)
-                .opacity(config.isHovered || !config.incognitoUnderline ? 1 : 0)
+                .opacity(config.isHovered || isPressed || !config.incognitoUnderline ? 1 : 0)
                 .animation(.easeOut, value: isEnabled)
         }
     }
