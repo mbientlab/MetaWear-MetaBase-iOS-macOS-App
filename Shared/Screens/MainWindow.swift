b5430e067c90039d451e564eb9781e70a390efc1
Ryan Ferrell <importRyan@gmail.com>
2022-01-12 22:46:44 -0800

    Insert onboarding sheet and footer for iOS

--- Shared/Screens/MainWindow.swift
+++ Shared/Screens/MainWindow.swift
@@ -5,7 +5,6 @@ import mbientSwiftUI
 /// The app's single window. A manual navigation stack is used instead of NavigationView across both iOS and macOS.
 struct MainWindow: View {
 
-    @AppStorage(wrappedValue: 0.0, UserDefaults.MetaWear.Keys.didOnboardAppVersion) private var didOnboard
     @EnvironmentObject private var routing: Routing
     @EnvironmentObject private var factory: UIFactory
 
@@ -15,24 +14,19 @@ struct MainWindow: View {
             .background(steadyHeaderBackground, alignment: .top)
             .animation(.easeOut, value: routing.destination)
             .foregroundColor(.myPrimary)
-        #if os(macOS)
+#if os(macOS)
             .toolbar { BluetoothErrorButton.ToolbarIcon() }
-        #elseif os(iOS)
-            .sheet(isPresented: showOnboardingSheet) {
-                Onboarding(importer: factory.makeImportVM(), vm: factory.makeOnboardingVM())
-            }
-        #endif
-    }
-
-    private var showOnboardingSheet: Binding<Bool> {
-        .init(get: { didOnboard < CurrentMetaBaseVersion },
-              set: { show in if !show { didOnboard = CurrentMetaBaseVersion } })
+#endif
     }
 
     private var stackNavigation: some View {
         ZStack {
             switch routing.destination {
-                case .choose:       ChooseDevicesScreen(routing, factory).transition(.add)
+                case .choose:
+                    ChooseDevicesScreen(routing, factory).transition(.add)
+#if os(iOS)
+                        .overlay(iOSFooter(), alignment: .bottom)
+#endif
                 case .history:      HistoryScreen(factory).transition(.add)
                 case .configure:    ConfigureScreen(factory).transition(.add)
                 case .log:          ActionScreen(factory).transition(.add)
