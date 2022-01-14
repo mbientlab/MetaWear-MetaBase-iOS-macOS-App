d0aac52aa3a8fae5bcb5e1668cf919f096f2d84e
Ryan Ferrell <importRyan@gmail.com>
2022-01-13 21:22:26 -0800

    Draft single-row grid

--- Shared/Screens/1_Choose/Choose+GridLandscape.swift
+++ Shared/Screens/1_Choose/Choose+GridLandscape.swift
@@ -1,6 +1,7 @@
 // Copyright 2021 MbientLab Inc. All rights reserved. See LICENSE.MD.
 
 import mbientSwiftUI
+import SwiftUI
 
 // MARK: - MacOS / iPad Wide Grid
 
@@ -11,22 +12,22 @@ extension ChooseDevicesScreen {
     struct WideOneRowGrid: View {
 
         @EnvironmentObject private var vm: DiscoveryListVM
+        @Environment(\.namespace) private var namespace
+        private var isBelowStaticCountLimit: Bool { vm.deviceCount <= staticCountLimit }
 
         var body: some View {
-            ScrollViewReader { scroller in
+            if isBelowStaticCountLimit {
+                grid
+                    .matchedGeometryEffect(id: "\(Self.self)", in: namespace!)
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+            } else {
                 ScrollView(.horizontal, showsIndicators: true) {
                     grid
-                        .padding(.leading, centeringPadding)
-                        .offset(y: -.verticalHoverDelta)
-                        .animation(.easeOut, value: centeringPadding)
-                        .animation(.easeOut, value: vm.groups)
-                        .animation(.easeOut, value: vm.ungrouped)
-                        .animation(.easeOut, value: vm.unknown)
+                        .matchedGeometryEffect(id: "\(Self.self)", in: namespace!)
                 }
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                .background(MeasureWidth(width: $windowWidth))
             }
-            .frame(maxHeight: .infinity, alignment: .bottom)
-            .background(measureWidth)
-
         }
 
         private var grid: some View {
@@ -35,17 +36,40 @@ extension ChooseDevicesScreen {
                     .frame(height: Self.macItemHeight, alignment: .bottom)
             }
             .frame(maxHeight: .infinity, alignment: .center)
+            .padding(.leading, centeringPadding)
+            .padding(.trailing, isBelowStaticCountLimit ? 0 : 50)
+            .offset(y: -.verticalHoverDelta)
+            .animation(.easeOut, value: centeringPadding)
+            .animation(.easeOut, value: vm.groups)
+            .animation(.easeOut, value: vm.ungrouped)
+            .animation(.easeOut, value: vm.unknown)
+#if os(iOS)
+            .animation(.easeOut.speed(2), value: windowWidth)
+#endif
         }
 
+
+#if os(iOS)
         @ViewBuilder private var sections: some View {
             DeviceIterator.KnownGroups()
-#if os(iOS)
+                .padding(.horizontal, CGFloat(iPhone: -.screenInset, 0))
+
             if showDividerA { divider }
-#endif
+
+            DeviceIterator.KnownUngrouped()
+                .padding(.horizontal, CGFloat(iPhone: -.screenInset, 0))
+
+            if showDividerB { divider }
+            DeviceIterator.UnknownNearby()
+        }
+#else
+        @ViewBuilder private var sections: some View {
+            DeviceIterator.KnownGroups()
             DeviceIterator.KnownUngrouped()
             if showDividerB { divider }
             DeviceIterator.UnknownNearby()
         }
+#endif
 
         // MARK: - Dividers
         @Environment(\.colorScheme) private var colorScheme
@@ -54,6 +78,7 @@ extension ChooseDevicesScreen {
                 .frame(width: 1)
                 .foregroundColor(colorScheme == .light ? .myPrimaryTinted.opacity(0.16) : .myGroupBackground)
                 .offset(y: .init(macOS: -.verticalHoverDelta - .screenInset / 2, iOS: .verticalHoverDelta))
+                .padding(.trailing, .init(iPhone: 20, 0))
         }
 
         private var showDividerA: Bool {
@@ -69,40 +94,39 @@ extension ChooseDevicesScreen {
         // MARK: - Layout Dimensions
 #if os(macOS)
         @State private var windowWidth = MainScene.minWidth
+        var staticCountLimit: Int {
+            let conservativeFit = windowWidth / (.deviceCellWidth * 2 + .screenInset)
+            return Int(max(3, conservativeFit - 1))
+        }
 #else
         @State private var windowWidth = UIScreen.main.bounds.width
+        let staticCountLimit: Int = idiom.is_iPhone ? 1 : Int(UIScreen.main.bounds.shortestSide / (.deviceCellWidth * 1.5))
 #endif
-        private var cellSpacing: CGFloat = .screenInset * 2
+        private let cellSpacing: CGFloat = .screenInset * 2
+
         private static let macItemHeight: CGFloat = 320
         private let rows = [GridItem(.fixed(Self.macItemHeight), spacing: 0, alignment: .bottom)]
-
-        private var measureWidth: some View {
-            GeometryReader { geo  in
-                Color.clear
-                    .onAppear { windowWidth = geo.size.width }
-                    .onChange(of: geo.size.width) { windowWidth = $0 }
-            }
-        }
     }
 }
 
 extension ChooseDevicesScreen.WideOneRowGrid {
 
     private var centeringPadding: CGFloat {
-        let usedSpace = contentWidth + .screenInset
+        if idiom.is_iOS { return isBelowStaticCountLimit ? 0 : .screenInset }
+        if isBelowStaticCountLimit { return 0 }
+        let usedSpace = contentWidth + .screenInset * 2
         let emptySpace = windowWidth - usedSpace
-        return emptySpace <= 0
-        ? .screenInset
-        : (emptySpace / 2)
+        return emptySpace <= 0 ? .screenInset : (emptySpace / 2)
     }
 
     private var contentWidth: CGFloat  {
         let cells = CGFloat(vm.deviceCount)
+        guard cells > 0 else { return .screenInset }
         let sections = CGFloat(countSections())
         let interItemSpacing = cellSpacing * (cells - 1)
-        let interSectionSpacing = (cellSpacing + 1) * (max(0, sections - 1))
-        let cellsContentWidth = .deviceCellWidth * cells
-        return interItemSpacing + interSectionSpacing + cellsContentWidth
+        let interSectionSpacing = (cellSpacing + .screenInset) * (max(0, sections - 1))
+        let cellsContentWidth = .deviceCellWidth * 1.25 * cells
+        return interItemSpacing + interSectionSpacing + cellsContentWidth + .screenInset
     }
 
     private func countSections() -> Int {
@@ -112,3 +136,15 @@ extension ChooseDevicesScreen.WideOneRowGrid {
     }
 }
 
+struct MeasureWidth: View {
+
+    @Binding var width: CGFloat
+
+    var body: some View {
+        GeometryReader { geo  in
+            Color.clear.hidden()
+                .onAppear { width = geo.size.width }
+                .onChange(of: geo.size.width) { if width != $0 { width = $0 } }
+        }
+    }
+}
