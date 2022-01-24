9d9a232e922e92441f824ae640d69d15c4da41b8
Ryan Ferrell <importRyan@gmail.com>
2022-01-24 14:22:52 -0800

    Update to latest SDK

--- Shared/Model/ActionType.swift
+++ Shared/Model/ActionType.swift
@@ -101,21 +101,23 @@ extension ActionType {
 
 fileprivate extension AnyPublisher where Output == MetaWear {
     func macro(_ config: ModulesConfiguration) -> MWPublisher<MWMacroIdentifier> {
-        self.macro(executeOnBoot: true) { metawear in
-            metawear
-                .optionallyLog(config.accelerometer)
-                .optionallyLog(config.altitude)
-                .optionallyLog(config.gyroscope)
-                .optionallyLog(byPolling: config.humidity)
-                .optionallyLog(config.ambientLight)
-                .optionallyLog(config.magnetometer)
-                .optionallyLog(config.pressure)
-                .optionallyLog(byPolling: config.thermometer)
-                .optionallyLog(config.fusionEuler)
-                .optionallyLog(config.fusionGravity)
-                .optionallyLog(config.fusionLinear)
-                .optionallyLog(config.fusionQuaternion)
-        }
+        self
+            .command(.macroStartRecording(runOnStartup: true))
+            .optionallyLog(config.accelerometer)
+            .optionallyLog(config.altitude)
+            .optionallyLog(config.gyroscope)
+            .optionallyLog(byPolling: config.humidity)
+            .optionallyLog(config.ambientLight)
+            .optionallyLog(config.magnetometer)
+            .optionallyLog(config.pressure)
+            .optionallyLog(byPolling: config.thermometer)
+            .optionallyLog(config.fusionEuler)
+            .optionallyLog(config.fusionGravity)
+            .optionallyLog(config.fusionLinear)
+            .optionallyLog(config.fusionQuaternion)
+            .command(.macroStopRecordingAndGenerateIdentifier)
+            .map(\.result)
+            .eraseToAnyPublisher()
     }
 }
 
